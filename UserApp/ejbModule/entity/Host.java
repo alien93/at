3bde9f783e46150928b4bf59fb7b3bf9cad8def8
Nina Marjanovic <nina.marjanovic@gmail.com>
2016-04-20 01:30:03 +0200

    new user app

---
+++ UserApp/ejbModule/entity/Host.java
@@ -0,0 +1,35 @@
+package entity;
+
+public class Host {
+
+	String address;
+	String alias;
+	public Host() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Host(String address, String alias) {
+		super();
+		this.address = address;
+		this.alias = alias;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getAlias() {
+		return alias;
+	}
+	public void setAlias(String alias) {
+		this.alias = alias;
+	}
+	@Override
+	public String toString() {
+		return "Host [address=" + address + ", alias=" + alias + "]";
+	}
+	
+	
+	
+}
